cd9171fdba34b850a33d859c1602fd9285d0b8a4
chidambarampriya <30342779+chidambarampriya@users.noreply.github.com>
2017-07-23 13:50:15 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n=10,m=15;
+printf("Enter the number");
+for(i=n;i<=m;i++)
+{
+if(n%2==1)
+printf("The number is odd");
+else
+printf("The is not odd number");
+}
+}
